e09d33dcf3473b08dc52d6b5c7a94e71b6c00e78
asathyagal18 <asathyagal18@simons-rock.edu>
2019-01-29 12:31:53 -0500

    Initial Commit

--- max_product_fast.c
+++ max_product_fast.c
@@ -1,6 +1,20 @@
 #include "max_product.h"
 
 int max_pairwise_product_fast(int *arr, int size){
-	return 0;
+
+	int index = 0;
+	for(int i = 1; i < size; i++){
+		if(arr[i] > arr[index]){index = i;}
+	}
+	int temp = arr[size-1];
+	arr[size-1] = arr[index];
+	arr[index] = temp;
+
+	index = 0;
+	for(int j = 1; j < size - 1; j++){
+		if(arr[j] > arr[index] && arr[j] != arr[size-1]){index = j;}
+	}
+
+	return arr[index] * arr[size-1];
 }
 
--- max_product_naive.c
+++ max_product_naive.c
@@ -1,5 +1,16 @@
 #include "max_product.h"
 
 int max_pairwise_product_naive(int * arr, int size){
-	return 0;
+
+	int product_max = 0;
+
+	for(int i = 0; i < size; i++){
+		for(int j = 0; j < size; j++){
+			if((arr[i] != arr[j]) && (arr[i] * arr[j] > product_max)){
+				product_max = arr[i] * arr[j];
+			}
+		}
+	}
+
+	return product_max;
 }
